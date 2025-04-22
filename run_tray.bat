399adffdd9ebac3b4d6135c52ae4812d98be9a15
Sasha11711 <oleksandrmarchuk2006@gmail.com>
2025-04-22 22:38:08 +0300

    Updated run_tray.bat

--- run_tray.bat
+++ run_tray.bat
@@ -2,16 +2,14 @@
 cd /d %~dp0
 if not exist venv (
     py -m pip install --upgrade pip
-    py -m pip install --upgrade virtualenv
     echo Creating virtual environment...
-    python -m venv venv
+    py -m venv venv
     echo Virtual environment created.
 )
 if exist venv (
     call venv\Scripts\activate.bat
     py -m pip install -r requirements.txt
     start /B pyw botTray.pyw
-)
-else (
+) else (
     echo Virtual environment not found.
 )
